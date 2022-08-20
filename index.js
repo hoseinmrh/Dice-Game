c22b624087d985ed2162c61e38250246e4625f1d
Hosein Mirhoseini <hoseinmirhoseini64@gmail.com>
2022-08-20 22:30:44 +0430

    Button added

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+document.getElementsByClassName("btn")[0].onclick = refreshFun;
 
-var randomDiceImage = "dice" + randomNumber1 + ".png";
+function refreshFun(){
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource = "images/" + randomDiceImage;
+  var randomDiceImage = "dice" + randomNumber1 + ".png";
 
-var image1 = document.querySelectorAll("img")[0];
+  var randomImageSource = "images/" + randomDiceImage;
 
-image1.setAttribute("src", randomImageSource);
+  var image1 = document.querySelectorAll("img")[0];
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomDiceImage2 = "dice" + randomNumber2 + ".png";
-var randomImageSource2 = "images/" + randomDiceImage2;
+  image1.setAttribute("src", randomImageSource);
 
-var image2 = document.querySelectorAll("img")[1];
-image2.setAttribute("src", randomImageSource2);
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+  var randomImageSource2 = "images/" + randomDiceImage2;
+
+  var image2 = document.querySelectorAll("img")[1];
+  image2.setAttribute("src", randomImageSource2);
 
 
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
-}
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+  }
 
-else if (randomNumber1 < randomNumber2){
-  document.querySelector("h1").innerHTML = "🚩 Play 2 Wins!";
-}
+  else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩 Player 2 Wins!";
+  }
 
-else{
-  document.querySelector("h1").innerHTML = "Draw!";
+  else{
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
 }
